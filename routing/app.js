58b08a709e12a1542f963bb90a926ba90bcb7242
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-10-21 17:29:13 +0530

    add mongoose

--- routing/app.js
+++ routing/app.js
@@ -1,5 +1,46 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb://test:test@ds035776.mlab.com:35776/expressjs'
+);
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var vijay = Person({
+    firstname: "vijay",
+    lastname: "deepak",
+    address: "chennai"
+});
+
+// save the user
+vijay.save(function(err) {
+    if(err) throw err;
+
+    console.log('User saved');
+});
+
+var prince = Person({
+    firstname: "prince",
+    lastname: "deepak",
+    address: "chilakaluripet"
+});
+
+// save the user
+prince.save(function(err) {
+    if(err) throw err;
+
+    console.log('User saved');
+});
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -14,6 +55,15 @@ app.set('view engine', 'ejs');
 // custom middleware
 app.use('/', function(req, res, next) {
     console.log(`Requested URL: ${req.url}`);
+
+    // get all the users
+    Person.find({}, function(err, users) {
+        if(err) throw err;
+
+        // object of all users
+        console.log(users);
+    });
+
     next(); // goes to next middleware like app.get()
 });
 
